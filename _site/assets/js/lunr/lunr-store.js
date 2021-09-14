3ff2562addbbe8d3fe2fd3b4e447049bd50e630c
JimTompkins <56934144+JimTompkins@users.noreply.github.com>
2021-09-14 15:18:38 -0300

    Added _site folder... trying to get missing blog post to show up on remote site

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Start of alpha test",
+        "excerpt":"The Android alpha test started today with a first sample delivered to a local tester: a friend, colleague and mentor who also consulted on the RF design of the discrete version.  He plays piano and digeridoo so I’m looking forward to some interesting videos.  ","categories": [],
+        "tags": [],
+        "url": "/_posts/2021-06-02-first-blog-entry/",
+        "teaser": null
+      },{
+        "title": "What's all this latency stuff, anyhow?",
+        "excerpt":"With a nod to Bob Pease (legendary analog circuit designer), today’s post looks at the latency from foot-tap to sound. Sound travels at 1400m/s or 1.4m/ms, so each ms of latency (aka delay), equates to the sound coming from 1m away. I’ve read some posts (such as this one)link related...","categories": [],
+        "tags": [],
+        "url": "/_posts/2021-07-20-latency/",
+        "teaser": null
+      },{
+        "title": "Audio Samples",
+        "excerpt":"The audio samples used by HappyFeet reside totally within the app; the hardware only sends a short message saying that a foot tap was detected. The audio samples come from the Polyphone website which has a library of soundfont files including these which we’ve used: Monsoon’s Maple Neck 1974 Fender...","categories": [],
+        "tags": [],
+        "url": "/_posts/2021-08-27-audio-samples/",
+        "teaser": null
+      }]
